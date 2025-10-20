7169a1da88228d9588585d91bd7c49fa73e32294
Bikash Shrestha <bikashshrestha@Bikashs-MBP.attlocal.net>
2025-10-19 20:28:06 -0500

    Initial Commit

--- Part3_C++.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int* ptr = new int(10);
-    cout << "C++ value: " << *ptr << endl;
-    delete ptr; // must manually free memory
-    return 0;
-}
